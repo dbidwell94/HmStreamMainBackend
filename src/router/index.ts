bc8bfd0e54f3dc84888a59524eecd328a9557c16
Devin Bidwell <devin.bidwell@accretivetg.com>
2021-01-07 18:16:34 -0800

    online status indicator

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,19 @@
-import Router from "koa-router";
+import Router, { RouterContext } from "koa-router";
 import UserRouter from "./userRoute";
 import AddressRouter from "./ipAddressRoute";
+import httpStatus from "http-status-codes";
 
 const apiRouter = new Router({ prefix: "/api" });
+
+apiRouter.get("/", async (ctx: RouterContext) => {
+  ctx.body = { status: "online" };
+  ctx.status = httpStatus.OK;
+});
 
 apiRouter.use("/users", UserRouter.routes());
 apiRouter.use("/address", AddressRouter.routes());
 
-export default {routes: apiRouter.routes(), methods: apiRouter.allowedMethods};
+export default {
+  routes: apiRouter.routes(),
+  methods: apiRouter.allowedMethods,
+};
